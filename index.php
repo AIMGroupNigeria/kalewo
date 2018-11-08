35b3e90dfc1b1787438fefd71fa0ab9cdc4ffb51
Seyi Onifade <xyluz@ymail.com>
2018-11-08 12:16:49 +0100

    getting started with code embed

--- index.php
+++ index.php
@@ -15,17 +15,53 @@
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
+
+
         <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <body class="text-center">
+             <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+
+                <header class="masthead mb-auto">
+                <div class="inner">
+                  <h3 class="masthead-brand">Cover</h3>
+                  <nav class="nav nav-masthead justify-content-center">
+                    <a class="nav-link active" href="#">Home</a>
+                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">Contact</a>
+                  </nav>
+                </div>
+              </header>
+ <main role="main" class="inner cover">
+            <script id="114063_c_478836" width="1920" height="1080" src="//player.dacast.com/js/player.js"  class="dacast-video"></script>
        
+          
+
+         
+            <h1 class="cover-heading">Cover your page.</h1>
+            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
+            </p>
+          </main>
+
+          <footer class="mastfoot mt-auto">
+            <div class="inner">
+              <p>Copyright, 2018</p>
+            </div>
+          </footer>
+        </div>
+
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
+
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+        <script src="../../assets/js/vendor/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
         <script>
