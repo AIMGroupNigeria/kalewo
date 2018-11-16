b551b557c7b3810e884b755f38e1f46fddd7fa98
Seyi Onifade <xyluz@ymail.com>
2018-11-16 10:48:51 +0100

    change players, add advert banner

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@ function stream(playerID, divId){
                 return player;
             }
 
-function delay( function() => {
-    var timer = 0;
-    return function(callback, ms) {
-        clearTimeout (timer);
-        timer = setTimeout(callback, ms);
-    };
-}();
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
